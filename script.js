2b3a6a330e83e01f35ea39362ba3636fbb3fd84e
febryntara <paglutgede@gmail.com>
2021-07-10 19:48:33 +0800

    hero section sekarang beneran final! lanjut ke section selanjutnya

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const navItems = document.querySelector(".nav-items");
 const heroButton = document.querySelector(".hero-button");
 const modal = document.querySelector(".modal-container");
 const modalClose = document.querySelector(".modal-form > span");
+const modalLoading = document.querySelector(".modal-loading");
+const modalAlertS = document.querySelector(".modal-alert-success");
+const modalAlertF = document.querySelector(".modal-alert-fail");
+
 
 navToggle.addEventListener("click",function(){
     navItems.classList.toggle("nav-restore")
@@ -22,8 +26,24 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxBg8dkmZf9EIKPE79OIq
 
   form.addEventListener('submit', e => {
     e.preventDefault()
+    modalLoading.classList.toggle("modal-do-loading");
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!'))
-      .catch(error => console.error('Error!'))
+      .then(response => {
+          console.log('Success!')
+          form.reset();
+          modalLoading.classList.toggle("modal-do-loading")
+          modalAlertS.classList.add("modal-do-loading")
+          setTimeout(function() {
+              modalAlertS.classList.remove("modal-do-loading")
+          }, 3000)
+        })
+      .catch(error => {
+          console.error('Error!')
+          modalAlertF.classList.add("modal-do-loading")
+          setTimeout(function() {
+              modalAlertF.classList.remove("modal-do-loading")
+          }, 3000)
+        })
+    
   })
 //   SECTION REST API END
